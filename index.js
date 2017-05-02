c05c915eeec5b7903bd4973098f932167a3d6d1c
ALiangLiang <david27072183@gmail.com>
2017-05-03 00:08:04 +0800

    新增 getEvent

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ const
   // 請求網頁的套件
   Request = require('request'),
   // promise 化 request 函數
-  request = function(options) {
+  request = function (options) {
     return new Promise((resolve, reject) => {
-      Request(options, function(error, response, body) {
-        if (error)
-          return reject(error)
+      Request(options, function (error, response, body) {
+        if (error) { return reject(error) }
         return resolve([response, body])
       })
     })
@@ -44,31 +43,32 @@ const api = {
    * @param {Boolean} [options.autoRelogin=false] - 當伺服器端 session 過期，是否自動重新登入
    * @returns {Boolean} 登入結果，true 或 false
    */
-  login: function(jar, userId, password, options = {}) {
+  login: function (jar, userId, password, options = {}) {
     const remember = options.remember || false
 
     jar.autoRelogin = options.autoRelogin || jar.autoRelogin || false
 
     return request({
-        method: 'POST',
-        url: 'http://aps.ncue.edu.tw/app/sess_student.php',
-        headers: {
-          'cache-control': 'no-cache',
-          'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'
-        },
-        formData: {
-          p_usr: userId || jar.userId,
-          p_pwd: password || jar.password
-        },
-        jar: jar
-      })
+      method: 'POST',
+      url: 'http://aps.ncue.edu.tw/app/sess_student.php',
+      headers: {
+        'cache-control': 'no-cache',
+        'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'
+      },
+      formData: {
+        p_usr: userId || jar.userId,
+        p_pwd: password || jar.password
+      },
+      jar: jar
+    })
       .then(([res, body]) => {
         if (body.search('成功') !== -1) {
           if (remember) {
             jar.userId = userId
             jar.password = password
-          } else
+          } else {
             jar.userId = jar.password = void 0
+          }
 
           return true
         } else if (body.search('錯誤') !== -1) {
@@ -85,7 +85,7 @@ const api = {
    * @param {RequestJar} jar - 一個jar
    * @returns {Boolean} 登出結果，true 或 false
    */
-  logout: function(jar) {
+  logout: function (jar) {
     return Promise.resolve(delete jar['_jar']['store']['idx']['aps.ncue.edu.tw']['/']['PHPSESSID'])
   },
 
@@ -96,24 +96,23 @@ const api = {
    * @param {RequestJar} jar - 一個已經登入的jar
    * @returns {String} 回傳成績頁面
    */
-  getScore: function(jar) {
+  getScore: function (jar) {
     return request({
-        method: 'GET',
-        url: 'http://aps.ncue.edu.tw/app/score.php',
-        headers: {
-          'cache-control': 'no-cache'
-        },
-        jar: jar
-      })
+      method: 'GET',
+      url: 'http://aps.ncue.edu.tw/app/score.php',
+      headers: {
+        'cache-control': 'no-cache'
+      },
+      jar: jar
+    })
       .then(([res, body]) => {
         if (body.search('請於首頁右上角之【設定】登入後使用') !== -1) {
-          if (jar.autoRelogin && jar.remember && jar.userId && jar.password)
+          if (jar.autoRelogin && jar.remember && jar.userId && jar.password) {
             return api.login(jar)
               .then((result) => (result) ?
                 api.Score(jar) :
                 Promise.reject(new Error('無法登入')))
-          else
-            throw new Error('尚未登入。')
+          } else { throw new Error('尚未登入。') }
         }
         return body
       })
@@ -133,13 +132,13 @@ const api = {
    */
 
   /**
-   * 取得通識講座列表
+   * 取得活動列表
    * @name getEvents
    * @method
    * @param {Stirng} [type=全部] - 活動種類：'全部', '通識', '心靈', '語文'
    * @returns {Event[]}
    */
-  getEvents: function(type = '全部') {
+  getEvents: function (type = '全部') {
     let selpp = (type === '通識') ? 1 :
       ((type === '心靈') ? 2 :
         ((type === '語文') ? 4 : 0))
@@ -149,7 +148,7 @@ const api = {
       headers: {
         'cache-control': 'no-cache'
       }
-    };
+    }
 
     return request(options)
       .then(([res, body]) => {
@@ -165,20 +164,64 @@ const api = {
             const children = $(row).children()
               .filter((child) => child.data !== ' '),
               date = new Date(children.eq(1).text())
-            date.setFullYear(date.getFullYear() + 1911);
+            date.setFullYear(date.getFullYear() + 1911)
             return {
               id: Number(children.find(':first-child > a').attr('href').match(/crs_seq=(\d*)/)[1]),
               name: children.find(':first-child > a').text(),
-              href: children.find(':first-child > a').attr('href'),
+              href: 'http://aps.ncue.edu.tw/app/' + children.find(':first-child > a').attr('href'),
               date: date,
               max: Number(children.eq(2).text()),
               cur: Number(children.eq(3).text()),
               status: children.eq(5).text(),
               signupUrl: children.find(':nth-child(6) > a').attr('href') || null,
-              menberListUrl: children.find(':nth-child(4) > a').attr('href') || null,
+              menberListUrl: children.find(':nth-child(4) > a').attr('href') || null
             }
           })
         return data.get()
+      })
+  },
+
+  /**
+   * 取得活動資訊
+   * @name getEvent
+   * @method
+   * @param {Number} eventId - 活動編號
+   * @returns {Event[]}
+   */
+  getEvent: function (eventId) {
+    const options = {
+      method: 'GET',
+      url: 'http://aps.ncue.edu.tw/app/show_crs.php?crs_seq=' + eventId,
+      headers: {
+        'cache-control': 'no-cache'
+      }
+    }
+
+    return request(options)
+      .then(([res, body]) => {
+        const
+          $ = cheerio.load(body, {
+            ignoreWhitespace: true,
+            xmlMode: false,
+            decodeEntities: false
+          }),
+          children = $('fieldset.ui-grid-a > div.ui-block-b > div > h1')
+          .filter((e) => e.data !== ' '),
+          date = new Date(children.eq(6).text()),
+          signupUrl = $('div[data-role=content] > a:nth-child(3)').attr('href')
+        date.setFullYear(date.getFullYear() + 1911)
+
+        return {
+          id: eventId,
+          name: children.eq(0).text(),
+          href: options.url,
+          date: date,
+          place: children.eq(4).text(),
+          max: Number(children.eq(8).text()),
+          status: $('div[data-role=content] > a:nth-child(3)').text(),
+          signupUrl: (signupUrl !== 'signup.php') ? signupUrl : null,
+          menberListUrl: 'http://aps.ncue.edu.tw/app/show_member.php?crs_seq=' + eventId
+        }
       })
   },
 
@@ -191,7 +234,7 @@ const api = {
    * @param {String} userId - 用戶學號
    * @returns {Boolean} 報名成功與否
    */
-  signupEvent: function(jar, eventId, userId) {
+  signupEvent: function (jar, eventId, userId) {
     const options = {
       method: 'POST',
       url: 'http://aps.ncue.edu.tw/app/sign_app_ok.php',
@@ -221,20 +264,21 @@ const api = {
 
     return request(options)
       .then(([res, body]) => {
-        if (body.search('名額已滿') !== -1)
+        if (body.search('名額已滿') !== -1) {
           throw new Error('名額已滿或報名已截止。')
-        else if (body.search('活動報名失敗') !== -1)
+        } else if (body.search('活動報名失敗') !== -1) {
           throw new Error('活動報名失敗。')
-        else if (body.search('您已經報名本活動了') !== -1)
+        } else if (body.search('您已經報名本活動了') !== -1) {
           throw new Error('已經報名本活動。')
-        else if (body.search('請於首頁右上角之【設定】登入後使用') !== -1) {
-          if (jar.autoRelogin && jar.remember && jar.userId && jar.password)
+        } else if (body.search('請於首頁右上角之【設定】登入後使用') !== -1) {
+          if (jar.autoRelogin && jar.remember && jar.userId && jar.password) {
             return api.login(jar)
               .then((result) => (result) ?
                 api.signupEvent(jar, eventId, userId) :
                 Promise.reject(new Error('無法登入')))
-          else
+          } else {
             throw new Error('尚未登入。')
+          }
         }
 
         return true
@@ -256,24 +300,23 @@ const api = {
    *   menberListUrl: (String | null)
    * }>}}
    */
-  getSignedupEvents: function(jar) {
+  getSignedupEvents: function (jar) {
     return request({
-        method: 'GET',
-        url: 'http://aps.ncue.edu.tw/app/finish.php',
-        headers: {
-          'cache-control': 'no-cache'
-        },
-        jar: jar
-      })
+      method: 'GET',
+      url: 'http://aps.ncue.edu.tw/app/finish.php',
+      headers: {
+        'cache-control': 'no-cache'
+      },
+      jar: jar
+    })
       .then(([res, body]) => {
         if (body.search('請於首頁右上角之【設定】登入後使用') !== -1) {
-          if (jar.autoRelogin && jar.remember && jar.userId && jar.password)
+          if (jar.autoRelogin && jar.remember && jar.userId && jar.password) {
             return api.login(jar)
               .then((result) => (result) ?
                 api.getSignedupEvents(jar) :
                 Promise.reject(new Error('無法登入')))
-          else
-            throw new Error('尚未登入。')
+          } else { throw new Error('尚未登入。') }
         }
 
         const
@@ -289,7 +332,7 @@ const api = {
             const children = $(row).children()
               .filter((child) => child.data !== ' '),
               date = new Date(children.eq(1).text())
-            date.setFullYear(date.getFullYear() + 1911);
+            date.setFullYear(date.getFullYear() + 1911)
             return {
               id: Number(children.find(':first-child > a').attr('href').match(/crs_seq=(\d*)/)[1]),
               name: children.find(':first-child > a').text(),
@@ -297,7 +340,7 @@ const api = {
               date: date,
               max: Number(children.eq(2).text()),
               cur: Number(children.eq(3).text()),
-              menberListUrl: children.find(':nth-child(4) > a').attr('href') || null,
+              menberListUrl: children.find(':nth-child(4) > a').attr('href') || null
             }
           })
         return data.get()
@@ -312,7 +355,7 @@ const api = {
    * @param {(String|Number)} eventId - 活動ID
    * @returns {Number} 報名編號
    */
-  getSignSeq: function(jar, eventId) {
+  getSignSeq: function (jar, eventId) {
     const options = {
       method: 'GET',
       url: 'http://aps.ncue.edu.tw/app/show_member.php?crs_seq=' + eventId,
@@ -325,8 +368,7 @@ const api = {
     return request(options)
       .then(([res, body]) => {
         const match = body.match(/Del_check\('(\d*)'\)/)
-        if (match.length < 1)
-          throw new Error('找不到報名編號。')
+        if (match.length < 1) { throw new Error('找不到報名編號。') }
         return Number(match[1])
       })
   },
@@ -339,7 +381,7 @@ const api = {
    * @param {(String|Number)} signSeq - 報名編號
    * @returns {Boolean} 是否成功取消報名
    */
-  cancelSignupEvent: function(jar, signSeq) {
+  cancelSignupEvent: function (jar, signSeq) {
     const options = {
       method: 'GET',
       url: 'http://aps.ncue.edu.tw/app/del_signup.php?sign_seq=' + signSeq,
@@ -352,17 +394,19 @@ const api = {
     return request(options)
       .then(([res, body]) => {
         if (body.search('請於首頁右上角之【設定】登入後使用') !== -1) {
-          if (jar.autoRelogin && jar.remember && jar.userId && jar.password)
+          if (jar.autoRelogin && jar.remember && jar.userId && jar.password) {
             return api.login(jar)
               .then((result) => (result) ?
                 api.cancelSignupEvent(jar, signSeq) :
                 Promise.reject(new Error('無法登入')))
-          else
+          } else {
             throw new Error('尚未登入。')
+          }
         }
 
-        if (body.search('已取消本筆活動報名資料') === -1)
+        if (body.search('已取消本筆活動報名資料') === -1) {
           return false
+        }
         return true
       })
   },
@@ -383,7 +427,7 @@ const api = {
    * @param {RequestJar} jar - 一個已經登入的jar
    * @returns {ApprovedGeneralEdu[]}
    */
-  getApprovedGeneralEduList: function(jar) {
+  getApprovedGeneralEduList: function (jar) {
     const options = {
       method: 'GET',
       url: 'http://aps.ncue.edu.tw/app/hour.php',
@@ -396,13 +440,12 @@ const api = {
     return request(options)
       .then(([res, body]) => {
         if (body.search('請於首頁右上角之【設定】登入後使用') !== -1) {
-          if (jar.autoRelogin && jar.remember && jar.userId && jar.password)
+          if (jar.autoRelogin && jar.remember && jar.userId && jar.password) {
             return api.login(jar)
               .then((result) => (result) ?
                 api.getApprovedGeneralEduList(jar) :
                 Promise.reject(new Error('無法登入')))
-          else
-            throw new Error('尚未登入。')
+          } else { throw new Error('尚未登入。') }
         }
 
         const
@@ -418,7 +461,7 @@ const api = {
               a = $(row),
               children = a.children(),
               date = new Date(children.eq(1).text().match(/\d*\/\d*\/\d*/))
-            date.setFullYear(date.getFullYear() + 1911);
+            date.setFullYear(date.getFullYear() + 1911)
             return {
               id: Number(a.attr('href').match(/crs_seq=(\d*)/)[1]),
               name: children.eq(0).text(),
@@ -429,7 +472,7 @@ const api = {
           })
         return data.get()
       })
-  },
+  }
 }
 
 module.exports = api
